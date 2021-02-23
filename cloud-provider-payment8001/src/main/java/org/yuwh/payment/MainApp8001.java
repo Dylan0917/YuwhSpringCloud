359aede51c1e7c23b02249f0c32fc695cfbac34a
CompanyPc <Dylan0917@users.noreply.github.com>
2021-02-23 10:30:24 +0800

    框架初始化

---
+++ cloud-provider-payment8001/src/main/java/org/yuwh/payment/MainApp8001.java
@@ -0,0 +1,16 @@
+package org.yuwh.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yu.wenhua
+ * @desc
+ * @date 2021/2/23 10:09
+ */
+@SpringBootApplication
+public class MainApp8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8001.class,args);
+    }
+}
